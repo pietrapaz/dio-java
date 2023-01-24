2b95cc656bc08e7c56d483f31d9224c448d244f7
Pietra <pietraplap@gmail.com>
2023-01-24 13:46:42 -0300

    adicionando operadores (incompleto

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,44 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //String nomeCompleto = "LINGUAGEM" + "JAVA";
+        //System.out.println(nomeCompleto);
+
+        //String concatenacao ="?"; 
+        //concatenacao = 1+1+1+"1";
+        //System.out.println(concatenacao);
+
+        //concatenacao = 1+"1"+1+1;
+        //System.out.println(concatenacao);
+
+        //concatenacao = 1+"1"+1+"1";
+        //System.out.println(concatenacao);
+
+        //concatenacao = "1"+1+1+1;
+        //System.out.println(concatenacao);
+
+        //concatenacao = "1"+(1+1+1);
+        //System.out.println(concatenacao);
+
+        //int numero = 5;
+
+        //numero = - numero;
+        //System.out.println(- numero);
+        //System.out.println(numero);
+
+        // repeticao
+        //int numero = 5;
+        //numero++;
+        //System.out.println(numero);
+
+        //System.out.println(numero++);
+        //System.out.println(numero);
+
+        //System.out.println(++numero);
+
+        boolean variavel = true;
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+}
