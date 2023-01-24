4fbe92b33fdda7a993eac667942b9f70ef629502
Pietra <pietraplap@gmail.com>
2023-01-24 13:01:54 -0300

    escrevendo alguns tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,14 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        // tipos primitivos
+        // estudar a classe string que representa texto na aplicacao
+          
         System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        double troco = 2.500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
